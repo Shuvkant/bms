12ee811912ae9886648d323e265cb7946c0f36dd
Shuvkant <weird426344@gmail.com>
2026-05-29 23:20:47 +0545

    ssh azurehuser

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hi there')
+  res.send('Hi there,this is the http server')
 })
 
 app.post('/signup', async (req, res) => {
